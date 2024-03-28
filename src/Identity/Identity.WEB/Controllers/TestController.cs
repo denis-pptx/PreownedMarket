7b199315e75c1450c9ab34747b1fa56472a1a1e5
Denis Konchik <denis.pptx@gmail.com>
2024-03-28 20:30:14 +0300

    added TestController

---
+++ src/Identity/Identity.WEB/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthServer.Api.Controllers;
+
+
+[Route("api/[controller]")]
+[ApiController]
+public class TestController : ControllerBase
+{
+
+    [HttpGet("test")]
+    public IActionResult Test()
+    {
+        var a = HttpContext.User;
+        return Ok();
+    }
+    
+    [HttpGet("authorize-test")]
+    [Authorize]
+    public IActionResult AuthorizedTest()
+    {
+        return Ok("authorized");
+    }
+}
