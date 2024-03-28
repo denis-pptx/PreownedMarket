2de4b2283cae6fb84ac8e24386a15dcd24548d3f
Denis Konchik <denis.pptx@gmail.com>
2024-03-28 20:19:34 +0300

    added messagin abstractions

---
+++ src/Identity/Identity.Application/Abstractions/Messaging/ICommandHandler.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Identity.Application.Abstractions.Messaging;
+
+public interface ICommandHandler<in TCommand, TResponse> 
+    : IRequestHandler<TCommand, TResponse> 
+    where TCommand : ICommand<TResponse>
+{
+}
