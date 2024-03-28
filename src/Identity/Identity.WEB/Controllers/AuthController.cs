fd84394f24ae9c02e4dd19f36147209e16f0d898
Denis Konchik <denis.pptx@gmail.com>
2024-03-28 20:24:42 +0300

    implemented auth controller

---
+++ src/Identity/Identity.WEB/Controllers/AuthController.cs
@@ -0,0 +1,41 @@
+﻿using Identity.Application.Features.Identity.Commands.LoginUser;
+using Identity.Application.Features.Identity.Commands.RefreshToken;
+using Identity.Application.Features.Identity.Commands.RegisterUser;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.WEB.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController(IMediator mediator) 
+    : ControllerBase
+{
+    // POST api/<AuthController>/register
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(RegisterUserCommand command, CancellationToken cancellationToken)
+    {
+        await mediator.Send(command, cancellationToken);
+
+        return Ok();
+    }
+
+    // POST api/<AuthController>/login
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(LoginUserCommand command, CancellationToken cancellationToken)
+    {
+        var loginUserVm = await mediator.Send(command, cancellationToken);
+
+        return Ok(loginUserVm);
+    }
+
+    // POST api/<AuthController>/refresh
+    [HttpPost("refresh")]
+    public async Task<IActionResult> RefreshToken(RefreshTokenCommand command, CancellationToken cancellationToken)
+    {
+        var refreshTokenVm = await mediator.Send(command, cancellationToken);
+
+        return Ok(refreshTokenVm);
+    }
+}
