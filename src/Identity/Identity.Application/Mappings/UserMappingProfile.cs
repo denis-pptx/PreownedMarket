49afaceb04a52ae36995f52048a54bb77c38bb53
Denis Konchik <denis.pptx@gmail.com>
2024-03-28 20:24:06 +0300

    added user mapping

---
+++ src/Identity/Identity.Application/Mappings/UserMappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Identity.Application.Features.Identity.Commands.RegisterUser;
+using Identity.Domain.Models;
+
+namespace Identity.Application.Mappings;
+
+public class UserMappingProfile : Profile
+{
+    public UserMappingProfile()
+    {
+        CreateMap<RegisterUserCommand, User>();
+    }
+}
