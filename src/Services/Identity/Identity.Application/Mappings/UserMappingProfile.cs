cc6fee5246b6b0a0f1a96e71dee77f6420d07565
Denis-HP <denis.pptx@gmail.com>
2024-04-01 13:34:50 +0300

    Properties in the mapper profile are set directly

--- src/Services/Identity/Identity.Application/Mappings/UserMappingProfile.cs
+++ src/Services/Identity/Identity.Application/Mappings/UserMappingProfile.cs
@@ -4,6 +4,8 @@ public class UserMappingProfile : Profile
 {
     public UserMappingProfile()
     {
-        CreateMap<RegisterUserCommand, User>();
+        CreateMap<RegisterUserCommand, User>()
+            .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.UserName))
+            .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email));
     }
 }
