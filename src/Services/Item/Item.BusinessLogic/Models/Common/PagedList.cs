e5e2ed57a93a4b410f7400e7a5b2f78e35e9cf34
Denis Konchik <denis.pptx@gmail.com>
2024-04-22 14:34:14 +0300

    changed PagedList class structure

--- src/Services/Item/Item.BusinessLogic/Models/Common/PagedList.cs
+++ src/Services/Item/Item.BusinessLogic/Models/Common/PagedList.cs
@@ -4,6 +4,14 @@ namespace Item.BusinessLogic.Models.Common;
 
 public class PagedList<T>
 {
+    public List<T> Items { get; }
+    public int Page { get; }
+    public int PageSize { get; }
+    public int TotalCount { get; }
+    public int TotalPages { get; }
+    public bool HasNextPage => Page * PageSize < TotalCount;
+    public bool HasPreviousPage => Page > 1;
+
     private PagedList(List<T> items, int page, int pageSize, int totalCount)
     {
         Items = items;
@@ -12,14 +20,6 @@ public class PagedList<T>
         TotalCount = totalCount;
         TotalPages = (int)Math.Ceiling(TotalCount / (double)pageSize);
     }
-
-    public List<T> Items { get; }
-    public int Page { get; }
-    public int PageSize { get; }
-    public int TotalCount { get; }
-    public int TotalPages { get; }
-    public bool HasNextPage => Page * PageSize < TotalCount;
-    public bool HasPreviousPage => Page > 1;
 
     public static async Task<PagedList<T>> CreateAsync(IQueryable<T> query, int page, int pageSize)
     {
