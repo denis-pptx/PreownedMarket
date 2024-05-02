8c12d23c065a57705876537bb38a0a8e0a55aa90
Denis Konchik <denis.pptx@gmail.com>
2024-05-03 01:14:06 +0300

    modified consumers

---
+++ src/Services/Item/Item.DataAccess/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Item.DataAccess.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Item.DataAccess.Repositories.Interfaces;
+
+public interface IUserRepository
+{
+    Task<User?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);   
+    void Add(User user);
+    void Delete(User user);
+}
