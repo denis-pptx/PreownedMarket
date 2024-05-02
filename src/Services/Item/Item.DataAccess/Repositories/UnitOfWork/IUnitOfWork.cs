f6a37e797fb44eac37f842b380328bf6cf4be6db
Denis Konchik <denis.pptx@gmail.com>
2024-05-02 18:17:34 +0300

    added unit of work

---
+++ src/Services/Item/Item.DataAccess/Repositories/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,6 @@
+﻿namespace Item.DataAccess.Repositories.UnitOfWork;
+
+public interface IUnitOfWork
+{
+    Task SaveChangesAsync(CancellationToken cancellationToken = default);
+}
---
+++ src/Services/Item/Item.DataAccess/Repositories/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿
+using Item.DataAccess.Data;
+
+namespace Item.DataAccess.Repositories.UnitOfWork;
+
+public class UnitOfWork(ApplicationDbContext _dbContext) 
+    : IUnitOfWork
+{
+    public async Task SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        await _dbContext.SaveChangesAsync(cancellationToken);
+    }
+}
